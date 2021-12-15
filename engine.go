29cd4b753372996508d872a4ac99decac25453f0
Michael Cabot <michael.a.cabot@gmail.com>
2021-12-15 23:24:37 +0100

    Start on engine

--- engine.go
+++ engine.go
@@ -4,3 +4,149 @@ package hypp
 var ssrNode = 1
 var textNode = 3
 var svgNS = "http://www.w3.org/2000/svg"
+
+func h(tag string, props HProps, children []VNode) VNode {
+    return VNode{
+        tag: tag,
+        props: props,
+        key: props.key(),
+        children: children,
+        kind: ssrNode,
+    }
+}
+
+func memo(view func(data interface{}) VNode, data interface{}) VNode {
+    return VNode{
+        memoView: view,
+        memo: data,
+    }
+}
+
+func text(value string, node Option[Node]) VNode {
+    return VNode{
+        tag: value,
+        kind: textNode,
+        node: node,
+    }
+}
+
+func dispatchInitializerID(dispatch Dispatch) Dispatch {
+    return dispatch
+}
+
+func dispatchID(dispatchable Dispatchable, payload Payload) {}
+
+func subscriptionsID[S any](state S) []Subscription[S] {
+    return nil
+}
+
+func renderID() {}
+
+func recycleNode(node VNode) VNode {
+    return node // TODO implement
+}
+
+func patchSubs[S any](oldSubs, newSubs []Subscription[S], dispatch Dispatch) []Subscription[S] {
+    return newSubs // TODO implement
+}
+
+func enqueue(render func(), busy bool) {
+    // TODO implement
+}
+
+func patch(
+    parentNode Node,
+    node Node,
+    vdomOld VNode,
+    vdom VNode,
+    listener func(VNode, Event),
+    busy bool,
+) Option[Node] {
+    return Option[Node]{
+        V: node, // TODO implement
+        OK: true,
+    }
+}
+
+type Event struct {
+    kind string // TODO implement
+}
+
+type EmptyState struct{}
+
+func (_ EmptyState) IAmDispatchable() {}
+
+func (a *AppProps[S]) init() {
+    if a.DispatchInitializer == nil {
+        a.DispatchInitializer = dispatchInitializerID
+    }
+    if a.Init == nil {
+        a.Init = EmptyState{}
+    }
+}
+
+func update[S comparable](props *AppProps[S], newState S) {
+    if (props.state != newState) {
+        props.state = newState
+        // if _, ok := props.state.(EmptyState); ok { // FIXME
+        //     props.dispatch = dispatchID
+        //     props.subscriptions = subscriptionsID[S]
+        //     props.render = renderID
+        // }
+        if props.Subscriptions != nil {
+            props.subs = patchSubs(props.subs, props.Subscriptions(props.state), props.dispatch)
+        }
+        if props.View != nil && !props.busy {
+            props.busy = true
+            enqueue(props.render, props.busy)
+        }
+    }
+}
+
+func app[S any](props AppProps[S]) Dispatch {
+    props.init()
+    var dispatch Dispatch
+
+    // props.vdom = props.Node.V // FIXME
+    // if props.Node.OK {
+    //     props.vdom = recycleNode(props.Node.V)
+    // }
+
+    listener := func(this VNode, event Event) {
+        // dispatch(this.events[event.kind], event) // FIXME
+    }
+
+    props.render = func() {
+        vdomOld := props.vdom
+        props.vdom = props.View(props.state)
+        props.busy = false
+        props.Node = patch(
+            props.Node.V.parentNode.V,
+            props.Node.V,
+            vdomOld,
+            props.vdom,
+            listener,
+            props.busy,
+        )
+    }
+
+    dispatch = func(dispatchable Dispatchable, props Payload) {
+		switch v := dispatchable.(type) {
+		case StateAndEffects[S]:
+			update(&props, v.State)
+			for _, effect := range v.Effects {
+				effect.Effecter(dispatch, effect.Payload)
+			}
+		case Action:
+			dispatch(v(props.state, props), nil)
+		case ActionAndPayload:
+			dispatch(v.Action, v.Payload)
+		default: // State
+			update(&props, v)
+		}
+	}
+	dispatch = dispatchInit(dispatch)
+	// dispatch(init, nil) // FIXME
+
+	return dispatch
+}
--- hypp.go
+++ hypp.go
@@ -2,11 +2,25 @@
 // This file is based on https://github.com/jorgebucaran/hyperapp/blob/main/index.d.ts
 package hypp
 
+import (
+    "fmt"
+)
+
 func App[S any](props AppProps[S]) Dispatch {
     return func(dispatchable Dispatchable, payload Payload) {}
 }
 
 type HProps map[string]interface{}
+
+func (h HProps) key() Option[string] {
+    if h == nil {
+        return Option[string]{}
+    }
+    if key, ok := h["key"]; ok {
+        return Option[string]{V: fmt.Sprintf("%v", key), OK: ok}
+    }
+    return Option[string]{}
+}
 
 func H(tag string, props HProps, children...VNode) VNode {
     return VNode{}
@@ -26,14 +40,27 @@ type Action[S any] func(state S, payload Payload) Dispatchable
 
 func (_ Action[S]) IAmDispatchable() {}
 
-type Node struct{}
+type Node struct{
+    parentNode Option[Node]
+}
+
+type Subscriptions[S any] func(state S) []Subscription[S]
+
+type Render func()
 
 type AppProps[S any] struct {
     Init Dispatchable
-    Subscriptions func(state S) []Subscription[S]
-    Dispatch func(dispatch Dispatch) Dispatch
-    view func(state S) VNode
-    node Node
+    Subscriptions Subscriptions[S]
+    DispatchInitializer func(dispatch Dispatch) Dispatch
+    View func(state S) VNode
+    Node Option[Node]
+
+    vdom VNode
+    dispatch Dispatch
+    subs []Subscription[S]
+    render Render
+    busy bool
+    state S
 }
 
 type Dispatch func(dispatchable Dispatchable, payload Payload)
@@ -68,13 +95,19 @@ type Subscription[S any] struct {
 
 type Unsubscribe func()
 
+type Option[T any] struct {
+    V T
+    OK bool
+}
+
 type VNode struct {
     props HProps
     children []VNode
-    node Node
+    node Option[Node]
     events map[string]interface{} // Action[S] | ActionAndPayload[S, P]
-    key *string
-    tag interface{} // string | func(data Indexable) VNode
+    key Option[string]
+    tag string
+    memoView func(data interface{}) VNode
     memo interface{} // Indexable
     kind int
 }
