f6ae14ffb1d9acfc0f92df97f895be4987e229a5
macabot <michael.a.cabot@gmail.com>
2022-01-03 20:52:41 +0100

    Implement the engine (#1)
    * Implement Todo-list example

---
+++ driver/js/js.go
@@ -0,0 +1,289 @@
+package js
+
+import (
+	"fmt"
+	"sync"
+	"syscall/js"
+
+	"github.com/macabot/hypp"
+)
+
+func document() js.Value {
+	return js.Global().Get("document")
+}
+
+var _ hypp.Driver = Driver{}
+
+type Driver struct{}
+
+func (d Driver) CreateTextNode(data string) hypp.Node {
+	return Node(document().Call("createTextNode", data))
+}
+
+func elementCreationOptionsToValue(options hypp.Option[hypp.ElementCreationOptions]) js.Value {
+	out := js.Undefined()
+	if options.OK {
+		out = js.ValueOf(map[string]interface{}{
+			"is": options.V.Is,
+		})
+	}
+	return out
+}
+
+func (d Driver) CreateElementNS(namespaceURI, qualifiedName string, options hypp.Option[hypp.ElementCreationOptions]) hypp.Node {
+	return Node(document().Call("createElementNS", namespaceURI, qualifiedName, elementCreationOptionsToValue(options)))
+}
+
+func (d Driver) CreateElement(tagName string, options hypp.Option[hypp.ElementCreationOptions]) hypp.Node {
+	return Node(document().Call("createElement", tagName, elementCreationOptionsToValue(options)))
+}
+
+func (d Driver) Enqueue(render func()) {
+	js.Global().Call("requestAnimationFrame", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		render()
+		return nil
+	}))
+}
+
+func hyppNodeToValue(node hypp.Node) js.Value {
+	if node == nil {
+		return js.Null()
+	}
+	return js.Value(node.(Node))
+}
+
+var _ hypp.Node = Node{}
+
+type Node js.Value
+
+func (n Node) ParentNode() hypp.Node {
+	return Node(js.Value(n).Get("parentNode"))
+}
+
+func (n Node) NodeType() int {
+	return js.Value(n).Get("nodeType").Int()
+}
+
+func (n Node) NodeValue() string {
+	return js.Value(n).Get("nodeValue").String()
+}
+
+func (n Node) SetNodeValue(nodeValue string) {
+	js.Value(n).Set("nodeValue", nodeValue)
+}
+
+func (n Node) NodeName() string {
+	return js.Value(n).Get("nodeName").String()
+}
+
+func (n Node) ChildNodes() []hypp.Node {
+	children := js.Value(n).Get("childNodes")
+	l := children.Length()
+	out := make([]hypp.Node, l)
+	for i := 0; i < l; i++ {
+		out[i] = Node(children.Index(i))
+	}
+	return out
+}
+
+func (n Node) InsertBefore(newNode, referenceNode hypp.Node) hypp.Node {
+	return Node(js.Value(n).Call(
+		"insertBefore",
+		hyppNodeToValue(newNode),
+		hyppNodeToValue(referenceNode),
+	))
+}
+
+func (n Node) RemoveChild(child hypp.Node) {
+	n.Events().(Events).deleteAll()
+	js.Value(n).Call("removeChild", hyppNodeToValue(child))
+}
+
+func (n Node) Get(name string) hypp.Option[interface{}] {
+	if !n.In(name) {
+		return hypp.Option[interface{}]{}
+	}
+	v := js.Value(n).Get(name)
+	kind := v.Type()
+	switch kind {
+	case js.TypeUndefined, js.TypeNull:
+		return hypp.Option[interface{}]{OK: true}
+	case js.TypeBoolean:
+		return hypp.Option[interface{}]{V: v.Bool(), OK: true}
+	case js.TypeNumber:
+		if js.Global().Get("Number").Call("isInteger", v).Bool() {
+			return hypp.Option[interface{}]{V: v.Int(), OK: true}
+		} else {
+			return hypp.Option[interface{}]{V: v.Float(), OK: true}
+		}
+	case js.TypeString:
+		return hypp.Option[interface{}]{V: v.String(), OK: true}
+	default:
+		panic(fmt.Errorf("js: cannot get Node property of type '%s'", kind))
+	}
+}
+
+func (n Node) In(name string) bool {
+	getPrototypeOf := js.Global().Get("Object").Get("getPrototypeOf")
+	for v := js.Value(n); !v.IsNull(); v = getPrototypeOf.Invoke(v) {
+		if v.Call("hasOwnProperty", name).Bool() {
+			return true
+		}
+	}
+	return false
+}
+
+func (n Node) Set(name string, value interface{}) {
+	js.Value(n).Set(name, value)
+}
+
+func (n Node) AppendChild(child hypp.Node) hypp.Node {
+	return Node(js.Value(n).Call("appendChild", hyppNodeToValue(child)))
+}
+
+func (n Node) RemoveEventListener(kind string, listener hypp.EventListener) {
+	js.Value(n).Call("removeEventListener", kind, js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		listener(Event(args[0]))
+		return nil
+	}))
+}
+
+func (n Node) AddEventListener(kind string, listener hypp.EventListener) {
+	js.Value(n).Call("addEventListener", kind, js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		listener(Event(args[0]))
+		return nil
+	}))
+}
+
+func (n Node) RemoveAttribute(name string) {
+	js.Value(n).Call("removeAttribute", name)
+}
+
+func (n Node) SetAttribute(name string, value interface{}) {
+	js.Value(n).Call("setAttribute", name, value)
+}
+
+func (n Node) Events() hypp.Events {
+	v := js.Value(n)
+	if v.Get("events").IsUndefined() {
+		js.Value(n).Set("events", map[string]interface{}{})
+	}
+	return Events(v.Get("events"))
+}
+
+func (n Node) Style() hypp.Style {
+	return Style(js.Value(n).Get("style"))
+}
+
+var _ hypp.Events = Events{}
+
+type Events js.Value
+
+type dispatchablesRepo struct {
+	mu sync.Mutex
+	i int
+	v map[int]hypp.Dispatchable
+}
+
+func (g *dispatchablesRepo) Add(value hypp.Dispatchable) int {
+	g.mu.Lock()
+	i := g.i
+	g.v[i] = value
+	g.i++
+	g.mu.Unlock()
+	return i
+}
+
+func (g *dispatchablesRepo) Set(i int, value hypp.Dispatchable) {
+	g.mu.Lock()
+	g.v[i] = value
+	g.mu.Unlock()
+}
+
+func (g *dispatchablesRepo) Del(i int) {
+	g.mu.Lock()
+	delete(g.v, i)
+	g.mu.Unlock()
+}
+
+func (g dispatchablesRepo) Get(i int) hypp.Dispatchable {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	return g.v[i]
+}
+
+var globalNodeEvents = dispatchablesRepo{v: map[int]hypp.Dispatchable{}}
+
+func (e Events) Set(name string, value hypp.Dispatchable) {
+	v := js.Value(e)
+	if p := v.Get(name); p.Type() == js.TypeNumber {
+		globalNodeEvents.Set(p.Int(), value)
+	} else {
+		i := globalNodeEvents.Add(value)
+		v.Set(name, i)
+	}
+}
+
+func (e Events) Get(name string) hypp.Dispatchable {
+	v := js.Value(e)
+	if p := v.Get(name); p.Type() == js.TypeNumber {
+		return globalNodeEvents.Get(p.Int())
+	}
+	return nil
+}
+
+func (e Events) Del(name string) {
+	v := js.Value(e)
+	if p := v.Get(name); p.Type() == js.TypeNumber {
+		globalNodeEvents.Del(p.Int())
+	}
+}
+
+func (e Events) deleteAll() {
+	names := js.Global().Get("Object").Call("keys", js.Value(e))
+	l := names.Length()
+	for i := 0; i < l; i++ {
+		name := names.Index(i).String()
+		e.Del(name)
+	}
+}
+
+var _ hypp.Event = Event{}
+
+type Event js.Value
+
+func (e Event) Type() string {
+	return js.Value(e).Get("type").String()
+}
+
+func (e Event) PreventDefault() {
+	js.Value(e).Call("preventDefault")
+}
+
+func (e Event) Target() hypp.EventTarget {
+	return EventTarget(js.Value(e).Get("target"))
+}
+
+var _ hypp.EventTarget = EventTarget{}
+
+type EventTarget js.Value
+
+func (e EventTarget) Value() string {
+	return js.Value(e).Get("value").String()
+}
+
+var _ hypp.Style = Style{}
+
+type Style js.Value
+
+func (s Style) SetProperty(propertyName, value string) {
+	js.Value(s).Call("setProperty", propertyName, value)
+}
+
+func (s Style) Set(name, value string) {
+	js.Value(s).Set(name, value)
+}
+
+func (s Style) Get(name string) string {
+	return js.Value(s).Get(name).String()
+}
--- hypp.go
+++ hypp.go
@@ -1,80 +1,213 @@
+//go:build go1.18
 // +build go1.18
+
 // This file is based on https://github.com/jorgebucaran/hyperapp/blob/main/index.d.ts
 package hypp
 
-func App[S any](props AppProps[S]) Dispatch {
-    return func(dispatchable Dispatchable, payload Payload) {}
+import (
+	"fmt"
+)
+
+type State interface {
+	comparable
+	Dispatchable
+}
+
+func App[S State](props AppProps[S]) Dispatch {
+	return app[S](props)
 }
 
 type HProps map[string]interface{}
 
-func H(tag string, props HProps, children...VNode) VNode {
-    return VNode{}
+func (h HProps) Key() Option[string] {
+	if key := h.Get("key"); key.OK {
+		return Option[string]{V: fmt.Sprint(key.V), OK: true}
+	}
+	return Option[string]{}
 }
 
-func Memo[D any](view func(data D) VNode, data D) VNode {
-    return VNode{}
+func (h HProps) Get(key string) Option[interface{}] {
+	if h == nil {
+		return Option[interface{}]{}
+	}
+	v, ok := h[key]
+	return Option[interface{}]{V: v, OK: ok}
 }
 
-func Text(value string) VNode {
-    return VNode{}
+func (h HProps) Has(key string) bool {
+	if h == nil {
+		return false
+	}
+	_, ok := h[key]
+	return ok
+}
+
+func (h *HProps) Set(key string, value interface{}) {
+	if *h == nil {
+		*h = HProps{}
+	}
+	m := *h
+	m[key] = value
+}
+
+func H(tag string, props HProps, children ...*VNode) *VNode {
+	return h(tag, props, children)
+}
+
+func Memo(view func(data interface{}) *VNode, data interface{}) *VNode {
+	return memo(view, data)
+}
+
+func Text(value string) *VNode {
+	return text(value, nil)
 }
 
 type Payload interface{}
 
-type Action[S any] func(state S, payload Payload) Dispatchable
+type ActionLike interface {
+	Dispatchable
+	iAmActionLike()
+}
+
+type Action[S State] func(state S, payload Payload) Dispatchable
 
 func (_ Action[S]) IAmDispatchable() {}
 
-type Node struct{}
+func (_ Action[S]) iAmActionLike() {}
 
-type AppProps[S any] struct {
-    Init Dispatchable
-    Subscriptions func(state S) []Subscription[S]
-    Dispatch func(dispatch Dispatch) Dispatch
-    view func(state S) VNode
-    node Node
+type Event interface {
+	// Dispatchable
+    Type() string
+	PreventDefault()
+	Target() EventTarget
+}
+
+type EventTarget interface {
+	Value() string
+}
+
+type EventListener func(Event)
+
+type Node interface {
+	ParentNode() Node
+	NodeType() int
+	NodeValue() string
+	SetNodeValue(nodeValue string)
+	NodeName() string
+	ChildNodes() []Node
+	InsertBefore(newNode, referenceNode Node) Node
+	RemoveChild(child Node)
+	Get(name string) Option[interface{}]
+	// In implements the in-operator
+	// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/in
+	In(name string) bool
+	Set(name string, value interface{})
+	AppendChild(child Node) Node
+	RemoveEventListener(kind string, listener EventListener)
+	AddEventListener(kind string, listener EventListener)
+	RemoveAttribute(name string)
+	SetAttribute(name string, value interface{})
+	Events() Events
+	Style() Style
+}
+
+type Events interface {
+	Set(name string, event Dispatchable)
+	Get(name string) Dispatchable
+	Del(name string)
+}
+
+type Style interface {
+	SetProperty(propertyName, value string)
+	Set(name, value string)
+	Get(name string) string
+}
+
+type Subscriptions[S State] func(state S) []Subscription[S]
+
+type Render func()
+
+type AppProps[S State] struct {
+	Driver Driver
+	Init                Dispatchable
+	Subscriptions       Subscriptions[S]
+	DispatchInitializer func(dispatch Dispatch) Dispatch
+	View                func(state S) *VNode
+	Node                Node
+
+	vdom     *VNode
+	dispatch Dispatch
+	subs     []Subscription[S]
+	render   Render
+	busy     bool
+	state    S
 }
 
 type Dispatch func(dispatchable Dispatchable, payload Payload)
 
 type Dispatchable interface {
-    IAmDispatchable()
+	IAmDispatchable()
 }
 
-type StateAndEffects[S any] struct {
-    State S
-    Effect []Effect[S]
+type StateAndEffects[S State] struct {
+	State   S
+	Effects []Effect[S]
 }
 
 func (_ StateAndEffects[S]) IAmDispatchable() {}
 
-type ActionAndPayload[S any] struct {
-    Action Action[S]
-    Payload Payload
+type ActionAndPayload[S State] struct {
+	Action  Action[S]
+	Payload Payload
 }
 
 func (_ ActionAndPayload[S]) IAmDispatchable() {}
 
-type Effect[S any] struct {
-    Effecter func(dispatch Dispatch, payload Payload)
-    Payload Payload
+func (_ ActionAndPayload[S]) iAmActionLike() {}
+
+type Effect[S State] struct {
+	Effecter func(dispatch Dispatch, payload Payload)
+	Payload  Payload
 }
 
-type Subscription[S any] struct {
-    Subscriber func(dispatch Dispatch, payload Payload) Unsubscribe
-    Payload Payload
+type Subscription[S State] struct {
+	Subscriber  func(dispatch Dispatch, payload Payload) Unsubscribe
+	Payload     Payload
+	unsubscribe Unsubscribe
+	Disabled    bool
 }
 
 type Unsubscribe func()
 
+type Option[T any] struct {
+	V  T
+	OK bool
+}
+
 type VNode struct {
-    props HProps
-    children []VNode
-    node Node
-    events map[string]interface{} // Action[S] | ActionAndPayload[S, P]
-    key *string
-    tag interface{} // string | func(data Indexable) VNode
-    memo interface{} // Indexable
-    kind int
+	props    HProps
+	children vKids
+	node     Node                  // Can be nil
+	events   map[string]ActionLike // Action[S] | ActionAndPayload[S]
+	key      Option[string]
+	tag      string
+	memoView func(data interface{}) *VNode
+	memo     interface{} // Indexable
+	kind     int
+}
+
+type vKids []*VNode
+
+func (v vKids) getKey(i int) Option[string] {
+	if i < len(v) {
+		return v[i].key
+	}
+	return Option[string]{}
+}
+
+func (v vKids) get(i int) *VNode {
+	if i < len(v) {
+		return v[i]
+	}
+	return nil
 }
