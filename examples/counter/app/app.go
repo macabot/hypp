55412fb68deac388198ee9ceccb20eb7c64f6b39
macabot <1422579+macabot@users.noreply.github.com>
2022-01-10 22:52:16 +0100

    Counter example (#7)

---
+++ examples/counter/app/app.go
@@ -0,0 +1,50 @@
+// This file is based on https://codepen.io/jorgebucaran/pen/MrBgMy
+package app
+
+import (
+    "github.com/macabot/hypp"
+    "github.com/macabot/hypp/tag/html"
+)
+
+type MyState struct {
+    hypp.EmptyState
+    count int
+}
+
+func (m MyState) clone() *MyState {
+    return &m
+}
+
+func subtract(state *MyState, _ hypp.Payload) hypp.Dispatchable {
+    newState := state.clone()
+    newState.count--
+    return newState
+}
+
+func add(state *MyState, _ hypp.Payload) hypp.Dispatchable {
+    newState := state.clone()
+    newState.count++
+    return newState
+}
+
+func Run(driver hypp.Driver, node hypp.Node) {
+    hypp.App[*MyState](hypp.AppProps[*MyState]{
+        Driver: driver,
+        Init: &MyState{},
+        View: func(state *MyState) *hypp.VNode {
+            return html.Main(
+                nil,
+                html.H1(nil, hypp.Textf("%d", state.count)),
+                html.Button(
+                    hypp.HProps{"onclick": hypp.Action[*MyState](subtract)},
+                    hypp.Text("ー"),
+                ),
+                html.Button(
+                    hypp.HProps{"onclick": hypp.Action[*MyState](add)},
+                    hypp.Text("＋"),
+                ),
+            )
+        },
+        Node: node,
+    })
+}
