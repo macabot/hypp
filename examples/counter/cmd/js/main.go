12f847320eb54687bb90cf73752ee5211eb94374
macabot <michael.a.cabot@gmail.com>
2022-01-10 22:52:16 +0100

    Counter example (#7)

---
+++ examples/counter/cmd/js/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"syscall/js"
+
+	jsd "github.com/macabot/hypp/driver/js"
+	"github.com/macabot/hypp/examples/counter/app"
+)
+
+func main() {
+	el := js.Global().Get("document").Call("getElementById", "app")
+	if el.IsNull() {
+		panic("Could not find element with id 'app'")
+	}
+	app.Run(
+		jsd.Driver{},
+		jsd.Node(el),
+	)
+
+	select {} // run Go forever
+}
