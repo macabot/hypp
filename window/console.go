e91298f4c9b9687040e85eb2244e6f482126c5ad
macabot <1422579+macabot@users.noreply.github.com>
2023-11-20 14:51:40 +0100

    Add window console methods

--- window/console.go
+++ window/console.go
@@ -12,8 +12,32 @@ func Console() Con {
 	return Con{js.Global().Get("console")}
 }
 
+// Debug outputs a debug message to the web console.
+// See https://developer.mozilla.org/en-US/docs/Web/API/console/debug_static
+func (c Con) Debug(args ...any) {
+	c.Value.Call("debug", args...)
+}
+
+// Info outputs an informational message to the web console.
+// See https://developer.mozilla.org/en-US/docs/Web/API/console/info_static
+func (c Con) Info(args ...any) {
+	c.Value.Call("info", args...)
+}
+
 // Log outputs a message to the web console.
 // See https://developer.mozilla.org/en-US/docs/Web/API/console/log
 func (c Con) Log(args ...any) {
 	c.Value.Call("log", args...)
+}
+
+// Warn outputs a warning message to the web console.
+// See https://developer.mozilla.org/en-US/docs/Web/API/console/warn_static
+func (c Con) Warn(args ...any) {
+	c.Value.Call("warn", args...)
+}
+
+// Error outputs an error message to the web console.
+// See https://developer.mozilla.org/en-US/docs/Web/API/console/error_static
+func (c Con) Error(args ...any) {
+	c.Value.Call("error", args...)
 }
