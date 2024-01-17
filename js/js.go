32113fd362fe4582d04cb3e68a9e06556ddd529d
macabot <1422579+macabot@users.noreply.github.com>
2024-01-17 20:52:13 +0100

    Add documentation

--- js/js.go
+++ js/js.go
@@ -38,34 +38,76 @@ func GetDriver() Driver {
 	return driver
 }
 
+// CopyBytesToGo copies bytes from src to dst.
+// It panics if src is not a Uint8Array or Uint8ClampedArray.
+// It returns the number of bytes copied, which will be the minimum of the lengths of src and dst.
 func CopyBytesToGo(dst []byte, src Value) int {
 	return d().CopyBytesToGo(dst, src)
 }
 
+// CopyBytesToJS copies bytes from src to dst.
+// It panics if dst is not a Uint8Array or Uint8ClampedArray.
+// It returns the number of bytes copied, which will be the minimum of the lengths of src and dst.
 func CopyBytesToJS(dst Value, src []byte) int {
 	return d().CopyBytesToJS(dst, src)
 }
 
+// FuncOf returns a function to be used by JavaScript.
+//
+// The Go function fn is called with the value of JavaScript's "this" keyword and the
+// arguments of the invocation. The return value of the invocation is
+// the result of the Go function mapped back to JavaScript according to ValueOf.
+//
+// Invoking the wrapped Go function from JavaScript will
+// pause the event loop and spawn a new goroutine.
+// Other wrapped functions which are triggered during a call from Go to JavaScript
+// get executed on the same goroutine.
+//
+// As a consequence, if one wrapped function blocks, JavaScript's event loop
+// is blocked until that function returns. Hence, calling any async JavaScript
+// API, which requires the event loop, like fetch (http.Client), will cause an
+// immediate deadlock. Therefore a blocking function should explicitly start a
+// new goroutine.
+//
+// Func.Release must be called to free up resources when the function will not be invoked any more.
 func FuncOf(fn func(this Value, args []Value) any) Func {
 	return d().FuncOf(fn)
 }
 
+// Global returns the JavaScript global object, usually "window" or "global".
 func Global() Value {
 	return d().Global()
 }
 
+// Null returns the JavaScript value "null".
 func Null() Value {
 	return d().Null()
 }
 
+// Undefined returns the JavaScript value "undefined".
 func Undefined() Value {
 	return d().Undefined()
 }
 
+// ValueOf returns x as a JavaScript value:
+//
+//	| Go                     | JavaScript             |
+//	| ---------------------- | ---------------------- |
+//	| js.Value               | [its value]            |
+//	| js.Func                | function               |
+//	| nil                    | null                   |
+//	| bool                   | boolean                |
+//	| integers and floats    | number                 |
+//	| string                 | string                 |
+//	| []interface{}          | new array              |
+//	| map[string]interface{} | new object             |
+//
+// Panics if x is not one of the expected types.
 func ValueOf(x any) Value {
 	return d().ValueOf(x)
 }
 
+// Error wraps a JavaScript error.
 type Error struct {
 	Value
 }
@@ -80,6 +122,7 @@ type FuncDriver interface {
 	Release()
 }
 
+// Func is a wrapped Go function to be called by JavaScript.
 type Func struct {
 	Value
 	driver FuncDriver
@@ -96,6 +139,9 @@ func (f Func) Driver() FuncDriver {
 	return f.driver
 }
 
+// Release frees up resources allocated for the function.
+// The function must not be invoked after calling Release.
+// It is allowed to call Release while the function is still running.
 func (f Func) Release() {
 	f.Driver().Release()
 }
@@ -183,86 +229,130 @@ func (v Value) Driver() ValueDriver {
 	return v.driver
 }
 
+// Bool returns the value v as a bool.
+// It panics if v is not a JavaScript boolean.
 func (v Value) Bool() bool {
 	return v.Driver().Bool()
 }
 
+// Call does a JavaScript call to the method m of value v with the given arguments.
+// It panics if v has no method m.
+// The arguments get mapped to JavaScript values according to the ValueOf function.
 func (v Value) Call(m string, args ...any) Value {
 	return v.Driver().Call(m, args...)
 }
 
+// Delete deletes the JavaScript property p of value v.
+// It panics if v is not a JavaScript object.
 func (v Value) Delete(p string) {
 	v.Driver().Delete(p)
 }
 
+// Equal reports whether v and w are equal according to JavaScript's === operator.
 func (v Value) Equal(w Value) bool {
 	return v.Driver().Equal(w)
 }
 
+// Float returns the value v as a float64.
+// It panics if v is not a JavaScript number.
 func (v Value) Float() float64 {
 	return v.Driver().Float()
 }
 
+// Get returns the JavaScript property p of value v.
+// It panics if v is not a JavaScript object.
 func (v Value) Get(p string) Value {
 	return v.Driver().Get(p)
 }
 
+// Index returns JavaScript index i of value v.
+// It panics if v is not a JavaScript object.
 func (v Value) Index(i int) Value {
 	return v.Driver().Index(i)
 }
 
+// InstanceOf reports whether v is an instance of type t according to JavaScript's instanceof operator.
 func (v Value) InstanceOf(t Value) bool {
 	return v.Driver().InstanceOf(t)
 }
 
+// Int returns the value v truncated to an int.
+// It panics if v is not a JavaScript number.
 func (v Value) Int() int {
 	return v.Driver().Int()
 }
 
+// Invoke does a JavaScript call of the value v with the given arguments.
+// It panics if v is not a JavaScript function.
+// The arguments get mapped to JavaScript values according to the ValueOf function.
 func (v Value) Invoke(args ...any) Value {
 	return v.Driver().Invoke(args...)
 }
 
+// IsNaN reports whether v is the JavaScript value "NaN".
 func (v Value) IsNaN() bool {
 	return v.Driver().IsNaN()
 }
 
+// IsNull reports whether v is the JavaScript value "null".
 func (v Value) IsNull() bool {
 	return v.Driver().IsNull()
 }
 
+// IsUndefined reports whether v is the JavaScript value "undefined".
 func (v Value) IsUndefined() bool {
 	return v.Driver().IsUndefined()
 }
 
+// Length returns the JavaScript property "length" of v.
+// It panics if v is not a JavaScript object.
 func (v Value) Length() int {
 	return v.Driver().Length()
 }
 
+// New uses JavaScript's "new" operator with value v as constructor and the given arguments.
+// It panics if v is not a JavaScript function.
+// The arguments get mapped to JavaScript values according to the ValueOf function.
 func (v Value) New(args ...any) Value {
 	return v.Driver().New(args...)
 }
 
+// Set sets the JavaScript property p of value v to ValueOf(x).
+// It panics if v is not a JavaScript object.
 func (v Value) Set(p string, x any) {
 	v.Driver().Set(p, x)
 }
 
+// SetIndex sets the JavaScript index i of value v to ValueOf(x).
+// It panics if v is not a JavaScript object.
 func (v Value) SetIndex(i int, x any) {
 	v.Driver().SetIndex(i, x)
 }
 
+// String returns the value v as a string.
+// String is a special case because of Go's String method convention. Unlike the other getters,
+// it does not panic if v's Type is not TypeString. Instead, it returns a string of the form "<T>"
+// or "<T: V>" where T is v's type and V is a string representation of v's value.
 func (v Value) String() string {
 	return v.Driver().String()
 }
 
+// Truthy returns the JavaScript "truthiness" of the value v. In JavaScript,
+// false, 0, "", null, undefined, and NaN are "falsy", and everything else is
+// "truthy". See https://developer.mozilla.org/en-US/docs/Glossary/Truthy.
 func (v Value) Truthy() bool {
 	return v.Driver().Truthy()
 }
 
+// Type returns the JavaScript type of the value v. It is similar to JavaScript's typeof operator,
+// except that it returns TypeNull instead of TypeObject for null.
 func (v Value) Type() Type {
 	return v.Driver().Type()
 }
 
+// A ValueError occurs when a Value method is invoked on
+// a Value that does not support it. Such cases are documented
+// in the description of each method.
 type ValueError struct {
 	Method string
 	Type   Type
