ee6b30384b8cc8a3983beeacb57aff026ec19de9
macabot <1422579+macabot@users.noreply.github.com>
2024-05-04 16:06:53 +0200

    Replace strings.Title with own title function

--- tag/cmd/generate-tags/main.go
+++ tag/cmd/generate-tags/main.go
@@ -20,11 +20,14 @@ var funcTemplate = `func %s(props hypp.HProps, children ...*hypp.VNode) *hypp.VN
 }
 `
 
+// main generates the tag functions.
+// The first argument is the package name.
+// All following arguments are the tag names. Each tag name must match `[a-z]+`.
 func main() {
 	packageName := os.Args[1]
 	funcs := make([]string, len(os.Args)-2)
 	for i, arg := range os.Args[2:] {
-		funcs[i] = fmt.Sprintf(funcTemplate, strings.Title(arg), arg)
+		funcs[i] = fmt.Sprintf(funcTemplate, title(arg), arg)
 	}
 
 	fmt.Printf(
@@ -32,4 +35,13 @@ func main() {
 		packageName,
 		strings.Join(funcs, "\n"),
 	)
+}
+
+func title(s string) string {
+	const diff = 'a' - 'A'
+	first := s[0]
+	if first < 'a' || first > 'z' {
+		panic("invalid tag name: " + s)
+	}
+	return string(first-diff) + string(s[1:])
 }
